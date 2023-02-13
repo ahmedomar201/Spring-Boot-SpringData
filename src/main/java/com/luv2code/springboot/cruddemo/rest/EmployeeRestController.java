a50d7d5f91147285cf722d922eca51d3a1bb4336
ahmed omar <https://github.com/ahmedomar201?tab=repositories>
2023-02-13 11:56:45 +0200

    update1

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -18,44 +18,42 @@ import com.luv2code.springboot.cruddemo.service.EmployeeService;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-	private EmployeeService employeeService; 
+
+	private EmployeeService employeeService;
 	
-	
-	//quick and dirty:inject employee DAO use constructor injection
 	@Autowired
 	public EmployeeRestController(EmployeeService theEmployeeService) {
-		theEmployeeService=employeeService;
+		employeeService = theEmployeeService;
 	}
 	
-	
-	//expose "/employee" and return list of employee
+	// expose "/employees" and return list of employees
 	@GetMapping("/employees")
-	public List<Employee> findAll(){
+	public List<Employee> findAll() {
 		return employeeService.findAll();
 	}
+
+	// add mapping for GET /employees/{employeeId}
 	
-	//add mapping for get/employees /{employee Id}
-	
-	@GetMapping("/employees /{employeeId}")
+	@GetMapping("/employees/{employeeId}")
 	public Employee getEmployee(@PathVariable int employeeId) {
-		 
-		Employee theEmployee =employeeService.findById(employeeId);
 		
-		 // throw expect if null
-		if(theEmployee==null) {
-			throw new RuntimeException("Employee id not found - "+employeeId);
-			
+		Employee theEmployee = employeeService.findById(employeeId);
+		
+		if (theEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
 		}
+		
 		return theEmployee;
 	}
 	
-	//add mapping for Post/employees add new employees
+	// add mapping for POST /employees - add new employee
 	
 	@PostMapping("/employees")
 	public Employee addEmployee(@RequestBody Employee theEmployee) {
 		
-		//also just in case they pass an id in JSON ...set id to 0
-		//this is to force save of new item ...instead of update 
+		// also just in case they pass an id in JSON ... set id to 0
+		// this is to force a save of new item ... instead of update
+		
 		theEmployee.setId(0);
 		
 		employeeService.save(theEmployee);
@@ -63,31 +61,42 @@ public class EmployeeRestController {
 		return theEmployee;
 	}
 	
-	//add mapping for put /employees updated new employees
+	// add mapping for PUT /employees - update existing employee
 	
 	@PutMapping("/employees")
-	public Employee updatedEmployee(@RequestBody Employee theEmployee) {
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
 		
 		employeeService.save(theEmployee);
 		
 		return theEmployee;
 	}
 	
-	//add mapping for deleted /employees delete  employees
-	@DeleteMapping
-	public String deleteEmployee(@PathVariable int employeeId ) {
+	// add mapping for DELETE /employees/{employeeId} - delete employee
+	
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
 		
-		Employee tempEmployee =employeeService.findById(employeeId);
+		Employee tempEmployee = employeeService.findById(employeeId);
 		
-		//// throw expect if null
-		if(tempEmployee==null) {
-			throw new RuntimeException("Employee id not found - "+employeeId);
-			
+		// throw exception if null
+		
+		if (tempEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
 		}
+		
 		employeeService.deleteById(employeeId);
 		
-		return "deleted employee id -"+employeeId;
+		return "Deleted employee id - " + employeeId;
 	}
 	
-
 }
+
+
+
+
+
+
+
+
+
+
